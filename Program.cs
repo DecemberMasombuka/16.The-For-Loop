407b8d8a72ff78c128670769f8cd04bf5d91dbf1
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:52:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.The_For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The While Loop");
+            int i = 0;  
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.WriteLine("The For Loop");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
